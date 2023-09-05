e9232d44fe549cec0c465404a2da858ccfc4a91d
AD1121 <sinagh30@gmail.com>
2023-09-05 15:30:37 +0200

    npm installation and code added

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+
+// Connecting to Mongodb
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Declaring routes
+app.use("/api/works", require("./routes/workRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
